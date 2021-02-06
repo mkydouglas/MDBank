5aa25f5a5abb083a03f84b381b35a26b556a33f2
Maycon Douglas <mkydouglas@outlook.com>
2021-02-06 17:19:21 -0300

    Organizando detalhes no layout

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -7,6 +7,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { LogoModule } from '../components/logo/logo.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    LogoModule
   ],
   declarations: [Tab2Page]
 })
